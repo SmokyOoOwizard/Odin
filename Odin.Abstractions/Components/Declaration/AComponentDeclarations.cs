9f523aa72439a370243de5c53a7d4aee47a3c33e
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2024-06-22 10:22:06 +0200

    Fix component declaration base

--- Odin.Abstractions/Components/Declaration/AComponentDeclarations.cs
+++ Odin.Abstractions/Components/Declaration/AComponentDeclarations.cs
@@ -3,9 +3,23 @@ using Odin.Abstractions.Components.Declaration.Builder.States;
 
 namespace Odin.Abstractions.Components.Declaration;
 
-public abstract class AComponentDeclarations
 {
+public abstract class AComponentDeclarations<T> where T : AComponentDeclarations<T>, new()
+{
+    public static T Instance { get; } = new();
+
+    public AComponentDeclarations()
+    {
+        // ReSharper disable once VirtualMemberCallInConstructor
+        Configure();
+    }
+
     protected abstract void Configure();
+
+    public ulong? GetComponentTypeId<TComponent>() where TComponent : IComponent
+    {
+        throw new System.NotImplementedException();
+    }
 
     public bool TryGet(ulong componentTypeId, out ComponentDeclaration componentDeclaration)
     {
