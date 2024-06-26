7413eeb055424d81953ba2bbb4ef4ee759df832e
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2024-06-26 08:17:30 +0200

    Fix type component id generation

--- Odin.Abstractions/Components/Utils/TypeComponentUtils.cs
+++ Odin.Abstractions/Components/Utils/TypeComponentUtils.cs
@@ -7,8 +7,8 @@ public static class TypeComponentUtils
 {
     public static ulong GetComponentTypeId<T>() where T : IComponent
     {
-        var name = typeof(T).FullName;
-        if(string.IsNullOrWhiteSpace(name))
+        var name = typeof(T).FullName?.Replace("+", ".");
+        if (string.IsNullOrWhiteSpace(name))
             throw new ArgumentException("Type must have a name", nameof(T));
 
         var byteString = Encoding.Default.GetBytes(name);
@@ -16,7 +16,26 @@ public static class TypeComponentUtils
         using var hasher = new SHA256Managed();
 
         var rawHash = hasher.ComputeHash(byteString);
-        
+
+        var hashCodeStart = BitConverter.ToUInt64(rawHash, 0);
+        var hashCodeMedium = BitConverter.ToUInt64(rawHash, 8);
+        var hashCodeEnd = BitConverter.ToUInt64(rawHash, 24);
+        var hashCode = hashCodeStart ^ hashCodeMedium ^ hashCodeEnd;
+
+        return hashCode;
+    }
+
+    public static ulong GetComponentTypeId(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentNullException(nameof(name));
+
+        var byteString = Encoding.Default.GetBytes(name);
+
+        using var hasher = new SHA256Managed();
+
+        var rawHash = hasher.ComputeHash(byteString);
+
         var hashCodeStart = BitConverter.ToUInt64(rawHash, 0);
         var hashCodeMedium = BitConverter.ToUInt64(rawHash, 8);
         var hashCodeEnd = BitConverter.ToUInt64(rawHash, 24);
