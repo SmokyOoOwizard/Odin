4f61fa1e1dc7b2ca669031808ae86e50a9827e75
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2024-06-26 02:09:50 +0200

    Add sqlite code gen

---
+++ Odin.Db.Sqlite.CodeGen.Sample/SecondTestComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Odin.Abstractions.Components;
+
+namespace Odin.Db.Sqlite.CodeGen.Sample;
+
+public struct SecondTestComponent : IComponent
+{
+    [IndexBy]
+    public Int32 TestField;
+}
---
+++ Odin.Db.Sqlite.CodeGen.Sample/TestComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Odin.Abstractions.Components;
+
+namespace Odin.Db.Sqlite.CodeGen.Sample;
+
+public struct TestComponent : IComponent
+{
+    public int TestField { get; set; }
+
+    public int CustomSetterGetter
+    {
+        get => TestField2;
+        set => TestField2 = value;
+    }
+
+    [IndexBy]
+    public Int32 TestField2;
+
+    public long TestField22;
+    public ulong TestField122;
+    public float TestField3;
+    public double TestField4;
+    public bool TestField5;
+
+    public string TestField6;
+
+    public int[] TestArray;
+}
