285e197ae37f41f5f429f77ad13e95fcce450f66
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2024-06-26 02:15:09 +0200

    Add rule: only public components

---
+++ Odin.Component.CodeGen/Diagnostics/Impl/ComponentPublicOnlyAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Odin.Component.CodeGen.Diagnostics.Impl;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class ComponentPublicOnlyAnalyzer : AComponentTypeDiagnosticAnalyzer
+{
+    private const string DIAGNOSTIC_ID = "ComponentRules_0006";
+    private const string CATEGORY = "Declaration";
+
+    private static readonly LocalizableString Message = "A component can only be a public";
+
+    private static readonly DiagnosticDescriptor Rule = new(DIAGNOSTIC_ID,
+                                                            Message,
+                                                            Message,
+                                                            CATEGORY,
+                                                            DiagnosticSeverity.Error,
+                                                            true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    protected override SyntaxKind[] SupportedSyntaxKinds => new[] { SyntaxKind.StructDeclaration };
+
+    protected override void Analyze(SyntaxNodeAnalysisContext context, ISymbol declaredSymbol)
+    {
+        var typeSymbol = declaredSymbol as INamedTypeSymbol;
+        if (typeSymbol == null)
+            return;
+
+        if (typeSymbol.DeclaredAccessibility == Accessibility.Public)
+            return;
+
+        var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation());
+        context.ReportDiagnostic(diagnostic);
+    }
+}
