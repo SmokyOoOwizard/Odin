d0da675b1491e8dc1f4303a2488ad183b5a5e864
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2024-07-08 16:06:02 +0200

    Add collector test

---
+++ Odin.Tests.Abstractions/Entities/AEntityRepositoryCollectorTests.cs
@@ -0,0 +1,53 @@
+﻿using Odin.Abstractions.Collectors.Matcher;
+using Odin.Abstractions.Components;
+using Odin.Abstractions.Entities;
+using Xunit;
+
+namespace Odin.Tests.Abstractions.Entities;
+
+public abstract class AEntityRepositoryCollectorTests
+{
+    private readonly IEntityRepository _repository;
+
+    protected AEntityRepositoryCollectorTests(IEntityRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public struct TestComponent : IComponent
+    {
+        public ulong TestData;
+    }
+
+    public class HasMatcher : AComponentMatcher
+    {
+        public override void Configure()
+        {
+            Filter().Has<TestComponent>();
+        }
+    }
+
+    public class NotMatcher : AComponentMatcher
+    {
+        public override void Configure()
+        {
+            Filter().Has<TestComponent>();
+        }
+    }
+
+    [Fact]
+    public void HasMatcherTest()
+    {
+        var collector = _repository.CreateCollector<HasMatcher>("Test");
+        _repository.CreateEntity();
+        var entity = _repository.CreateEntity();
+
+        _repository.Replace(entity, new TestComponent());
+
+        var entities = collector.GetBatch()
+                                .GetEntities()
+                                .ToArray();
+
+        Assert.Equal(1, entities.Length);
+    }
+}
---
+++ OdinSdk.Tests/InMemoryEntityRepositoryCollectorTests.cs
@@ -0,0 +1,11 @@
+﻿using Odin.Tests.Abstractions.Entities;
+using OdinSdk.Entities;
+
+namespace OdinSdk.Tests;
+
+public class InMemoryEntityRepositoryCollectorTests : AEntityRepositoryCollectorTests
+{
+    public InMemoryEntityRepositoryCollectorTests() : base(new InMemoryEntityRepository())
+    {
+    }
+}
