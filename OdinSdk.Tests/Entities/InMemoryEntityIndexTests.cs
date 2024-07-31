e7c2e4cc58534db7aea3f0ba69a8e3a1c481a272
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2024-07-31 18:35:18 +0200

    Add in memory index tests

---
+++ OdinSdk.Tests/Entities/InMemoryEntityIndexTests.cs
@@ -0,0 +1,13 @@
+﻿using Odin.Tests.Abstractions.Entities;
+using OdinSdk.Contexts;
+
+namespace OdinSdk.Tests.Entities;
+
+public class InMemoryEntityIndexTests : AEntityIndexTests
+{
+    public InMemoryEntityIndexTests() : base(
+        new InMemoryEntityContext(nameof(InMemoryEntityIndexTests))
+    )
+    {
+    }
+}
